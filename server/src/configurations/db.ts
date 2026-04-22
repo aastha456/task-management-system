28fbd24fa707f6f01ba61a64f9c38aa7697ae55e
aastha456 <tamrakaraastha7@gmail.com>
2026-04-22 15:57:52 +0545

    Initialize server setup

---
+++ server/src/configurations/db.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { config } from '../config';
+
+const connectDB = async () => {
+    try {
+        if(!config.MONGO_URI){
+            throw new Error("MONGO_URI is not defined")
+
+        }
+
+        await mongoose.connect(config.MONGO_URI);
+        console.log("Connected to DB");
+
+    } catch(error) {
+        console.log("Error connect to DB:", error);
+        process.exit();
+
+    }
+}
+
+export default connectDB;
---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from 'cors';
+import { config} from './config';
+import connectDB from "./configurations/db";
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(config.PORT, () => {
+    console.log(`\nServer is running on port ${config.PORT} \n`)
+
+})
