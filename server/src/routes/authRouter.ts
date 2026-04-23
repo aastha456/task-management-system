bc9989fb9b5e8384bb8095c67de8b43436f4792b
aastha456 <tamrakaraastha7@gmail.com>
2026-04-23 10:17:38 +0545

    Implement login logic n service

--- server/src/routes/authRouter.ts
+++ server/src/routes/authRouter.ts
@@ -4,7 +4,7 @@ import * as authController from "../controllers/authController"
 const router = Router();
 
 router.post("/register", authController.register);
-router.post("/login", () => {});
+router.post("/login", authController.login);
 router.post("/refresh-token", () => {});
 router.post("/logout", () => {});
 
--- server/src/services/authServices.ts
+++ server/src/services/authServices.ts
@@ -1,5 +1,7 @@
+import { NUMBER_OF_SALT_ROUNDS } from "../constants/auth";
 import { UserAuthRequest, UserLoginRequest } from "../interfaces/user"
 import UserModel from "../models/UserModel";
+import bcrypt from "bcrypt";
 
 
 export const register = async (data: UserAuthRequest) => {
@@ -10,14 +12,32 @@ export const register = async (data: UserAuthRequest) => {
         throw new Error("User already exists")
     }
 
-    console.log({ name, email, password});
-    return await UserModel.create({ name, email, password});
+    const hashedPassword = await bcrypt.hash(password, NUMBER_OF_SALT_ROUNDS)
+
+    return await UserModel.create({ name, email, password: hashedPassword});
  
 
 }
 
 export const login = async (data: UserLoginRequest ) => {
     const { email, password } = data;
-    
+    const user = await UserModel.findOne( {email }).select("+password");
+    if(!user){
+       throw new Error("User not found");
+    }
 
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid){
+        throw new Error("Password is invalid");
+
+    }
+        
+    return { 
+        user: {
+            id: user._id,
+            name: user.name,
+            email: user.email
+        }
+    }
+    
 }
