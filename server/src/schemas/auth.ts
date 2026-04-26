18d8640c7343cc288a00871571ba58f6e675eb11
aastha456 <tamrakaraastha7@gmail.com>
2026-04-26 15:26:48 +0545

    Fixed the minor mistake in schema

--- server/src/schemas/auth.ts
+++ server/src/schemas/auth.ts
@@ -1,14 +1,14 @@
 import { z } from 'zod';
 
-export const register = z.object({
+export const registerSchema = z.object({
   name: z.string().min(3),
-  email: z.email(),
+  email: z.string().email(),
   password: z.string().min(6),
   role: z.enum(["admin", "user"]).optional()
 });
 
 export const loginSchema = z.object({
-    email: z.email(),
+    email: z.string().email(),
     password: z.string()
 })
 
