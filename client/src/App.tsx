2081c0d5c4952666ef43842239dcd989a063f618
aastha456 <tamrakaraastha7@gmail.com>
2026-05-01 23:29:34 +0545

    Add the toastify css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
 import Router from './Router'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   return (
     <>
     <Router/>
+
+    <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="light"
+      />
     </>
   )
 }
