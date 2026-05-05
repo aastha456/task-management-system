55b4b0b64932d9110db358d2c50afaf22f50467f
aastha456 <tamrakaraastha7@gmail.com>
2026-05-05 23:08:52 +0545

    Modified the interface for the task

--- client/src/interfaces/task.ts
+++ client/src/interfaces/task.ts
@@ -1,3 +1,36 @@
+// export interface Task {
+//     _id: string;
+//     title: string;
+//     description?: string;
+//     status: "todo" | "in-progress" | "done";
+//     priority: "low" | "medium" | "high";
+//     dueDate?: string;
+//     assignedTo?: string;
+//     createdBy: string;
+//     projectId?: string;
+//     workspaceId: string;
+    
+// }
+
+// // export type TaskForm = Omit<Task, "_id" | "createdBy">;
+
+// export interface TaskForm {
+//     title: string;
+//     description?: string;
+//     priority: "low" | "medium" | "high";
+//     status: "todo" | "in-progress" | "done";
+//     dueDate?: string;
+//     workspaceId: string;    // ✅ required
+//     projectId?: string;     // ✅ optional
+//     assignedTo?: string;    // ✅ optional
+// }
+
+export interface UserLite {
+    _id: string;
+    name: string;
+    email?: string;
+}
+
 export interface Task {
     _id: string;
     title: string;
@@ -5,8 +38,21 @@ export interface Task {
     status: "todo" | "in-progress" | "done";
     priority: "low" | "medium" | "high";
     dueDate?: string;
-    assignedTo?: string;
-    createdBy: string;
+
+   assignedTo?: UserLite | string;
+   createdBy?: UserLite | string;
+
+    projectId?: string;
+    workspaceId: string;
 }
 
-export type TaskForm = Omit<Task, "_id" | "createdBy">;
+export interface TaskForm {
+    title: string;
+    description?: string;
+    priority: "low" | "medium" | "high";
+    status: "todo" | "in-progress" | "done";
+    dueDate?: string;
+    workspaceId: string;
+    projectId?: string;
+    assignedTo?: string;
+}
