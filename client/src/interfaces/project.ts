bca3fd79c8e7ae824804a501e4efaf4577260169
aastha456 <tamrakaraastha7@gmail.com>
2026-05-05 23:05:50 +0545

    Refactor the interface design of project

--- client/src/interfaces/project.ts
+++ client/src/interfaces/project.ts
@@ -2,6 +2,20 @@ export interface Project {
     _id: string;
     name: string;
     description?: string;
-    workspace: string;
-    createdBy: string;
+    workspace: {
+        _id: string;
+        name: string;
+    };
+    createdBy: {
+        _id: string;
+        name: string;
+        email: string;
+    };
+    createdAt: string;
 }
+
+export type CreateProjectPayload = {
+  name: string;
+  description?: string;
+  workspace: string;
+};
